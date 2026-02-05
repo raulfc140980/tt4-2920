9195164b8e71d01d8a0ad8afe1de426ba8f426bc
Raul Francia <refc_80@hotmail.com>
2026-02-04 22:59:09 -0500

    directives in app.component.html

--- my-app-angular/src/app/app.ts
+++ my-app-angular/src/app/app.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('my-app-angular');
+  title = 'my-app-angular';
+  name = 'Raul';
+  imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJffy5BZAhq_f19LjrAPi-Idsx05wdFOHUjAs'
+  isLoggedIn = true;
+  items = ["item A", "item B", "item C", "item D", "item E", "item F", "item G"];
+  isActive = false
+  textColor = 'red';
+  fontSize = '50px';
+
+
+clickButton() {
+    console.log("Button Pressed");
+    this.name = 'Messi';
+    this.imageUrl = 'https://www.istockphoto.com/photo/button-blue-blank-gm154401486-17919852';  
+    this.isActive = true;
+    this.textColor = 'yellow';
+    this.fontSize--;
+  }
+
+logout(){
+    this.isLoggedIn = false;
+  }
+
+login(){
+    this.isLoggedIn = true;
+  }
 }
